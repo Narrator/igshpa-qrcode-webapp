e2e87c205e3fb1d9e042828e11af1e9657690676
Kaushik Subramaniam <kaushik.subman@gmail.com>
2017-03-12 15:54:06 -0500

    Fix image to dara url

--- src/app/Members/controllers/MemberDetails-controller.js
+++ src/app/Members/controllers/MemberDetails-controller.js
@@ -223,6 +223,20 @@ angular.module('VettiverFaxApp').controller('MemberDetailsCtrl',
           };
       }());
 
+      function toDataUrl(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+          var reader = new FileReader();
+          reader.onloadend = function() {
+            callback(reader.result);
+          }
+          reader.readAsDataURL(xhr.response);
+        };
+        xhr.open('GET', url);
+        xhr.responseType = 'blob';
+        xhr.send();
+      }
+
       $scope.generateBadge = function(member) {
         Member.prototype$generateBadge({
           id: member.id
