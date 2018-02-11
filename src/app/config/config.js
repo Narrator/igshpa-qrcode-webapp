2d2bcd2f0d4fe05a84a646b9b0a4773e33031312
Kaushik Subramaniam <kaushik.subman@gmail.com>
2018-02-11 00:26:11 -0600

    Deployment change

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,2 +1,2 @@
 angular.module("VettiverFaxApp.config", [])
-.constant("EnvironmentConfig", {"API_URL":"http://localhost:4000/api"});
+.constant("EnvironmentConfig", {"API_URL":"http://139.78.55.193:4000/api"});
